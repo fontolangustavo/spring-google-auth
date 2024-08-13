0397c33272a45b9e682ccc78fa2bb6e3e2463327
Fontolan <fontolangustavo@gmail.com>
2024-08-13 17:24:50 -0300

    fix: adjust user constructor

--- src/main/java/com/fontolan/spring/google/auth/domains/User.java
+++ src/main/java/com/fontolan/spring/google/auth/domains/User.java
@@ -1,10 +1,14 @@
 package com.fontolan.spring.google.auth.domains;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Entity
 @Table(name = "users")
